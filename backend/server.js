fa14df20dc2ec64e8b6b745e420b05f723a2b38b
aldin_mikara@hotmail.com <aldin_mikara@hotmail.com>
2021-08-05 18:48:16 +0200

    setup basic example

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
-const express = require("express");
-const http = require("http").Server(express());
-const socketIo = require("socket.io")(http);
+const app = require('express')();
+const http = require("http").Server(app);
+const socketIo = require("socket.io")(http, {
+  cors: {
+    origin: '*',
+  }
+});
 
 let position = {
   x: 200,
@@ -9,8 +13,28 @@ let position = {
 
 socketIo.on("connection", socket => {
   socket.emit("position", position);
+  socket.on("move", data => {
+    switch(data){
+      case "left":
+        position.x -= 5;
+        socketIo.emit('position', position);
+        break;
+      case "right":
+        position.x += 5;
+        socketIo.emit('position', position);
+        break;
+      case "up":
+        position.y -= 5;
+        socketIo.emit('position', position);
+        break;
+      case "down":
+        position.y += 5;
+        socketIo.emit('position', position);
+        break;
+    }
+  })
 })
 
-http.listen(300, () => {
+http.listen(3000, () => {
   console.log('running server on port 3000')
 })
